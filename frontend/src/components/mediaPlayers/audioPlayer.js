ef2a0ccb791028dac19a959a91864568740c7c6c
Misachu10032 <johnzhou.engineer@gmail.com>
2023-10-04 01:24:30 -0600

    media players

---
+++ frontend/src/components/mediaPlayers/audioPlayer.js
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react';
+
+
+const AudioPlayer = ( {src,audioRef}) => {
+
+
+  return (
+    <div>
+      <audio ref={audioRef} >
+        <source src={src} type="audio/mpeg" />
+        Your browser does not support the video tag.
+      </audio>
+     
+    </div>
+  );
+};
+
+export default AudioPlayer;
---
+++ frontend/src/components/mediaPlayers/videoPlayer.js
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react';
+
+
+const VideoPlayer = ( {src,videoRef}) => {
+
+
+  return (
+    <div>
+      <video ref={videoRef} >
+        <source src={src} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+     
+    </div>
+
+    
+  );
+};
+
+export default VideoPlayer;
